47b6266f6edb7aeeb8d693ea1dcabda9e2a42193
Johan Sjoberg <johansjoberg88@gmail.com>
2021-03-11 15:06:00 +0100

    Tersser added

--- snowpack.config.js
+++ snowpack.config.js
@@ -24,5 +24,24 @@ module.exports = {
         manifest: true,
         target: 'es2020',
     },
-    plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv', '@snowpack/plugin-sass'],
+    plugins: [
+        '@snowpack/plugin-react-refresh',
+        '@snowpack/plugin-dotenv',
+        '@snowpack/plugin-sass',
+        [
+            'snowpack-plugin-terser',
+            {
+                /**
+                 * @see Plugin Options below
+                 */
+                terserOptions: {
+                    compress: {
+                        arguments: true,
+                        passes: 2,
+                        unsafe_arrows: true,
+                    },
+                },
+            },
+        ],
+    ],
 };
